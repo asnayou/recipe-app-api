c0278f9aeaf5cdad5833869667562d836979112f
asnayou <youri.asnacios@gmail.com>
2023-07-10 14:20:48 +0200

    correct tests files

--- app/app/calc.py
+++ app/app/calc.py
@@ -11,3 +11,13 @@ def add(x, y):
         y (_type_): _description_
     """
     return x+y
+
+
+def substract(x, y):
+    """substract
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+    """
+    return y-x
